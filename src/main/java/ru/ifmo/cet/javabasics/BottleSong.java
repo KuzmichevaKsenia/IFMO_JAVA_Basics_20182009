4188b91e30ef12fa51ce9cffd0f0b57d5f20102b
KuzmichevaKsenia <kuzmichevaky@mail.ru>
2018-10-03 00:39:57 +0300

    made all tests passing

--- src/main/java/ru/ifmo/cet/javabasics/BottleSong.java
+++ src/main/java/ru/ifmo/cet/javabasics/BottleSong.java
@@ -32,12 +32,52 @@ package ru.ifmo.cet.javabasics;
  */
 public class BottleSong {
 
-    public BottleSong(int bottleTakenAtOnce) {
-        //TODO
+    private static final int intCount = 99;
+    private static final String[] digits = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    private static final String[] numbers = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+    private static final String[] tens = {"zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+    private int intBottleTakenAtOnce;
+    private String stringBottleTakenAtOnce;
+    private String s = ""; //окончание
+
+    public BottleSong(int intBottleTakenAtOnce) {
+        if (intBottleTakenAtOnce > 0 && intBottleTakenAtOnce < 100) {
+            this.intBottleTakenAtOnce = intBottleTakenAtOnce;
+            stringBottleTakenAtOnce = getStringNumber(intBottleTakenAtOnce);
+        } else throw new IllegalArgumentException();
     }
 
     public String getBottleSongLyrics() {
-        //TODO
-        throw new UnsupportedOperationException();
+        String str = "";
+        int localIntCount = intCount;
+        while (localIntCount > intBottleTakenAtOnce) {
+            str += localIntCount + " bottles of beer on the wall, " + localIntCount + " bottles of beer.\n" +
+                    "Take " + stringBottleTakenAtOnce + " down and pass around, ";
+            localIntCount -= intBottleTakenAtOnce;
+            if (localIntCount != 1) s = "s";
+            str += localIntCount + " bottle" + s + " of beer on the wall.\n";
+            s = "";
+        }
+        String finalStringBottleTakenAtOnce = getStringNumber(localIntCount);
+        if (localIntCount != 1) s = "s";
+        str += localIntCount + " bottle" + s + " of beer on the wall, " + localIntCount + " bottle" + s + " of beer.\n" +
+                "Take " + finalStringBottleTakenAtOnce + " down and pass around, no more bottles of beer on the wall.\n" +
+                "No more bottles of beer on the wall, no more bottles of beer.\n" +
+                "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
+        s = "";
+        return str;
     }
+
+    private String getStringNumber(int number) {
+        String str;
+        if (number < 10) str = digits[number];
+        else if (number < 20) str = numbers[number - 10];
+        else {
+            str = tens[number / 10];
+            if (number % 10 != 0)
+                str += " " + digits[number % 10];
+        }
+        return str;
+    }
+
 }
